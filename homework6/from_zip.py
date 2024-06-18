88967fd4b9b3b46a87c3321e2772a51e836bf219
VladSkopenko <skopirka2k17@gmail.com>
2024-06-18 03:11:46 +0300

    complete homework 6

--- homework6/from_zip.py
+++ homework6/from_zip.py
@@ -1,13 +1,12 @@
 import zipfile
 
-# Путь к архиву
+
 zip_file_path = 'data.zip'
 
 
 extract_to_path = 'data'
 
-# Открытие и разархивирование
+
 with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
     zip_ref.extractall(extract_to_path)
 
-print(f"Files have been extracted to {extract_to_path}")
